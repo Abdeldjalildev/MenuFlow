8c0e5f60bdcaa7fec7afa9ddfa9d9e788d11c1ef
Abdeldjalil Khalfa <abdeldjalilkhalfa2@gmail.com>
2026-08-31 22:24:02 +0100

    security: expose Firebase app for callable functions

--- src/firebase.ts
+++ src/firebase.ts
@@ -11,6 +11,6 @@ const firebaseConfig = {
   appId: "1:958824525746:web:83207f591dcacc1b647eab"
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
