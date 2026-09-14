90b213f3ed6877c3351f30560d10bdd764eb408a
KHALFA abdeldjalil <abdeldjalil.khalfa@enssmal.edu.dz>
2026-09-14 21:02:56 +0100

    fix: expose Firebase Functions client for waiter calls

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDzxje8PRLZlvvuBk8fI7aOm_5ZzLmMeHo",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const functions = getFunctions(app, 'us-central1');
