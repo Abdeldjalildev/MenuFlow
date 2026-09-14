72bc1d9c12d4da637deb3605665e4ff459ce1222
KHALFA abdeldjalil <abdeldjalil.khalfa@enssmal.edu.dz>
2026-09-14 15:43:03 +0100

    feat: expose canonical secure order mutations

--- functions/phase9CreateOrder.js
+++ functions/phase9CreateOrder.js
@@ -1,5 +1,7 @@
 const legacyFunctions = require('./index');
 const { createOrder } = require('./canonicalOrderCreation');
+const { mutateOrder } = require('./secureOrderMutations');
 
 legacyFunctions.createOrder = createOrder;
+legacyFunctions.mutateOrder = mutateOrder;
 module.exports = legacyFunctions;
